166c54e36dc9c11e224d0652484c652fcd77a5bb
SilviaHidalgo <55782560+SilviaHidalgo@users.noreply.github.com>
2019-09-30 12:47:43 +0200

    Update and rename ejercicio3.java to Ejercicio3.java

--- Tema1/Ejercicio3.java
+++ Tema1/Ejercicio3.java
@@ -1,4 +1,4 @@
-public class ejercicio3 { 
+public class Ejercicio3 { 
   public static void main(String[] args){
     System.out.printf(" %-20s %-20s\n", "red","rojo");
     System.out.printf(" %-20s %-20s\n", "rice","arroz");
