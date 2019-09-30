3b3b9366d6d41ea9c9ae21b6c4999fb3c3f30b7d
SilviaHidalgo <55782560+SilviaHidalgo@users.noreply.github.com>
2019-09-30 12:49:24 +0200

    Update and rename ejercicio7.java to Ejercicio7.java

--- Tema1/Ejercicio7.java
+++ Tema1/Ejercicio7.java
@@ -1,4 +1,4 @@
-public class ejercicio7 { 
+public class Ejercicio7 { 
   public static void main(String[] args){
     System.out.println(" ************ ");
     System.out.println(" ***** ****** ");
