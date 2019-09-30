2abd13a8c1994929c20eb4b0bb04b7997afdc025
SilviaHidalgo <55782560+SilviaHidalgo@users.noreply.github.com>
2019-09-30 12:27:15 +0200

    Update ejercicio2.java

--- Tema2/ejercicio2.java
+++ Tema2/ejercicio2.java
@@ -1,4 +1,4 @@
-public class Ejercicio2 { 
+public class ejercicio2 { 
   public static void main(String[] args){
     String nombre = "Silvia Hidalgo Moreno";
     System.out.println("Hola, soy "+nombre);
