0c69a62c289308ed8f4c34e8c8a57055bcea6331
SilviaHidalgo <55782560+SilviaHidalgo@users.noreply.github.com>
2019-09-30 22:25:43 +0200

    Add files via upload

---
+++ Tema4/Ejercicio11.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Ejercicio11 {
+  public static void main (String[] args){
+    
+    System.out.println("Este programa te dice que horoscopo eres");
+    
+    Scanner x = new Scanner(System.in);
+    
+    System.out.println("Escribe la hora: ");
+    
+    int horas = x.nextInt();
+    
+    Scanner k = new Scanner(System.in);
+    
+    System.out.println("Escribe los minutos: ");
+     
+    int minutos = k.nextInt();
+     
+    int medianoche = 86400;
+    int segundos;
+    
+    segundos = ((horas*60)*60)+(minutos*60);
+    
+    if (horas!=0 && minutos!=0){
+      
+        System.out.println("Quedan "+(medianoche-segundos)+" segundos hasta la medianoche.");
+      
+      }
+      
+    else{
+      
+      System.out.println("Ya es media noche.");
+      
+      }
+    
+    }
+  }
