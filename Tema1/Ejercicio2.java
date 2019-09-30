0d18744d190367ab53778350c7d43395ffd97d91
SilviaHidalgo <55782560+SilviaHidalgo@users.noreply.github.com>
2019-09-30 12:47:11 +0200

    Update and rename ejercicio2.java to Ejercicio2.java

--- Tema1/Ejercicio2.java
+++ Tema1/Ejercicio2.java
@@ -1,4 +1,4 @@
-public class ejercicio2 { 
+public class Ejercicio2 { 
   public static void main(String[] args){
     System.out.println("Me llamo Silvia Hidalgo Moreno");
     System.out.println("Vivo en campanillas.");
