eb1e415f77ee1f04f3f7266096d46858970a1dbb
SilviaHidalgo <55782560+SilviaHidalgo@users.noreply.github.com>
2019-10-01 09:43:12 +0200

    Add files via upload

---
+++ Tema4/Ejercicio05.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ejercicio05{
+  public static void main (String[] args){
+    
+    System.out.println("Este programa resuelve ecuaciones del tipo ax+b=0");
+    
+    Scanner d = new Scanner(System.in);
+    
+    System.out.println("Escribe un valor para a: ");
+    
+      double a = d.nextDouble();
+    
+    System.out.println("Escribe un valor para b: ");
+     
+     double b = d.nextDouble();
+     double x;
+     
+    if(a!=0){
+      
+      x = -b/a;
+      
+      System.out.println("x es igual a: "+x);
+      
+      }
+      
+    else{
+      
+      System.out.println("Esta ecuación no tiene una solución real");
+      
+      }
+    }
+  }
