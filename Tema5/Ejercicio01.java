4e85121881e7ef5c01e87b26340807c1a016ad4d
SilviaHidalgo <55782560+SilviaHidalgo@users.noreply.github.com>
2019-10-09 09:15:02 +0200

    Add files via upload

---
+++ Tema5/Ejercicio01.java
@@ -0,0 +1,13 @@
+public class Ejercicio01{
+  public static void main (String[] args){
+  
+  int i;
+    
+    for(i=0 ; i<=100; i=i+5){
+      
+      System.out.println(i);
+    
+    }
+  
+  }
+}
